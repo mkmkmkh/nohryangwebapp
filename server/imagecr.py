29f3ace24ea8c5e04301b59a9ba8c600c0b3839a
eojinchoi <choi_eo_jin@naver.com>
2023-02-01 10:57:00 +0900

    auto push

--- server/imagecr.py
+++ server/imagecr.py
@@ -30,7 +30,7 @@ driver.set_window_size(1500, 900)
 
 # 해당 요소가 로딩이 완료될때까지 최대 n초 대기 하는 'Explicit Waits'
 WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-    (By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'))).send_keys('ej_python')
+    (By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'))).send_keys('ej_python2')
 # 기다리지 않고 그냥 가져오는 코드
 driver.find_element(
     by=By.XPATH, value='//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys('zxc0312')
@@ -140,7 +140,7 @@ driver.implicitly_wait(5)
 urllib.request.urlretrieve(img_url[0], 'real' + ".jpg")
 driver.implicitly_wait(5)
 
-driver.close()
+driver.quit()
 
 
 # for index, link in enumerate(img_url):
